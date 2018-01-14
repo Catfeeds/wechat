999e5567013fe7c072ac2ca7bd9dc663eae5e1f8
王东升 <wangds_6@hotmail.com>
2018-01-14 19:48:48 +0800

    测试

--- ad/source/suggest/display.ctrl.php
+++ ad/source/suggest/display.ctrl.php
@@ -21,6 +21,9 @@ if($do == 'display'){
     if(!empty($keyword)){
         $where .= " AND (name LIKE '%{$keyword}%' OR content LIKE '%{$keyword}%')";
     }
+    if($_W['ad_type'] != 1){
+        $where .= " AND uid IN (SELECT uid FROM ".tablename('mc_members')." WHERE uniacid='{$_W['uniacid']}' AND province='{$_W['province']}' AND city='{$_W['city']}')";
+    }
     $list = pdo_fetchall("SELECT * FROM ".tablename('sj_news_suggest')." WHERE {$where} ORDER BY id DESC LIMIT {$pindex},{$psize}");
     $pager = pagination(pdo_fetchcolumn("SELECT COUNT(1) FROM ".tablename('sj_news_suggest')." WHERE {$where}"),$page,$psize);
 }
